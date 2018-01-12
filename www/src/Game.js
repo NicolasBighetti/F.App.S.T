d6317fcc2f098ec691d17c9bfbf3202c1d991588
Unknown <nicobighetti@gmail.com>
2018-01-12 22:15:13 +0100

    P2P gameplay

--- www/src/Game.js
+++ www/src/Game.js
@@ -1,4 +1,6 @@
-Ball.Game = function(game) {};
+Ball.Game = function(game) {
+	this.game = game;
+};
 Ball.Game.prototype = {
 	create: function() {
 		this.physics.startSystem(Phaser.Physics.ARCADE);
@@ -12,8 +14,43 @@ Ball.Game.prototype = {
 		this.movementForce = 10;
 		this.ballStartPos = { x: Ball._WIDTH*0.5, y: 450 };
 
+		this.lastMessage = "";
 
-		var sockTest = io('http://10.212.115.16:8080');
+		this.sockTest = io('http://10.212.115.16:8080');
+		this.sockTest.emit('login', 'yili');
+		this.sockTest.on('message', (message) => {
+
+		});
+
+		this.sockTest.emit('message', "33%");
+		this.clientCircle = this.game.add.graphics(0, 0);
+		this.peerCircle = this.game.add.graphics(0,0);
+// graphics.lineStyle(2, 0xffd900, 1);
+
+		var opts = {peerOpts: {trickle: false}, autoUpgrade: false};
+		this.p2psocket = new P2P(this.sockTest, opts, () => {
+			this.p2psocket.emit('peer-obj', 'Hello there. I am ' + this.p2psocket.peerId)
+		});
+
+		this.p2psocket.upgrade();
+
+		this.p2psocket.on('peer-obj', (data) => {
+  		console.log(data);
+			this.lastMessage = JSON.stringify(data);
+		});
+
+		this.p2psocket.on('coord-peer', (coord) => {
+			console.log(coord);
+			this.peerCoord = coord;
+		});
+		/*this.sockTest.emit('connectTo', 'Smartphone');
+		this.sockTest.on('connectTo', (ip) => {
+			this.phoneSock = io(ip);
+			this.phoneSock.on('touch', (coord) => {
+					this.printTouch(coord);
+			});
+		});*/
+
 		this.timerText = this.game.add.text(15, 15, "Time: "+this.timer, this.fontBig);
 		this.keys = this.game.input.keyboard.createCursorKeys();
 
@@ -21,14 +58,28 @@ Ball.Game.prototype = {
 		window.addEventListener("deviceorientation", this.handleOrientation, true);
 
 		this.time.events.loop(Phaser.Timer.SECOND, this.updateCounter, this);
-
+		this.time.events.loop(Phaser.Timer.SECOND, this.sendCoord, this);
 	},
 	updateCounter: function() {
 		this.timer++;
-		this.timerText.setText("Time: "+this.timer);
+		this.timerText.setText(this.lastMessage+" "+this.timer);
+	},
+	sendCoord: function() {
+		this.lastX = this.game.input.x;
+		this.lastY = this.game.input.y;
+		this.p2psocket.emit('coord-peer', {x: this.lastX, y:this.lastY});
 	},
 	update: function() {
-
+		if(this.lastX && this.lastY){
+			this.clientCircle.clear();
+			this.clientCircle.beginFill(0xFF0000, 1);
+			this.clientCircle.drawCircle(this.lastX, this.lastY, 10);
+		}
+		if(this.peerCoord){
+			this.peerCircle.clear();
+			this.peerCircle.beginFill(0x00FF00, 1);
+			this.peerCircle.drawCircle(this.peerCoord.x, this.peerCoord.y, 10);
+		}
 	},
 	handleOrientation: function(e) {
 		// Device Orientation API
@@ -40,5 +91,7 @@ Ball.Game.prototype = {
 	render: function() {
 		// this.game.debug.body(this.ball);
 		// this.game.debug.body(this.hole);
+		//console.log('x : ' + this.game.input.x + 'y : ' + this.game.input.y);
+
 	}
 };
