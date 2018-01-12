39a4bcc8d45dcbcc26d5e916be909cc19879ac8f
Unknown <nicobighetti@gmail.com>
2018-01-12 11:43:02 +0100

    Trim project for multi_playerPOC

--- www/src/Game.js
+++ www/src/Game.js
@@ -15,9 +15,8 @@ Ball.Game.prototype = {
 		this.movementForce = 10;
 		this.ballStartPos = { x: Ball._WIDTH*0.5, y: 450 };
 
-		this.pauseButton = this.add.button(Ball._WIDTH-8, 8, 'button-pause', this.managePause, this);
-		this.pauseButton.anchor.set(1,0);
-		this.pauseButton.input.useHandCursor = true;
+		this.serverSocket = new WebSocket('ws://localhost:8080');
+
 		this.audioButton = this.add.button(Ball._WIDTH-this.pauseButton.width-8*2, 8, 'button-audio', this.manageAudio, this);
 		this.audioButton.anchor.set(1,0);
 		this.audioButton.input.useHandCursor = true;
@@ -38,9 +37,6 @@ Ball.Game.prototype = {
 		this.physics.enable(this.ball, Phaser.Physics.ARCADE);
 		this.ball.body.setSize(18, 18);
 		this.ball.body.bounce.set(0.3, 0.3);
-
-		this.initLevels();
-		this.showLevel(1);
 		this.keys = this.game.input.keyboard.createCursorKeys();
 
 		Ball._player = this.ball;
@@ -58,96 +54,17 @@ Ball.Game.prototype = {
 		this.borderGroup.setAll('body.immovable', true);
 		this.bounceSound = this.game.add.audio('audio-bounce');
 	},
-	initLevels: function() {
-		this.levels = [];
-		this.levelData = [
-			[
-				{ x: 96, y: 224, t: 'w' }
-			],
-			[
-				{ x: 72, y: 320, t: 'w' },
-				{ x: 200, y: 320, t: 'h' },
-				{ x: 72, y: 150, t: 'w' }
-			],
-			[
-				{ x: 64, y: 352, t: 'h' },
-				{ x: 224, y: 352, t: 'h' },
-				{ x: 0, y: 240, t: 'w' },
-				{ x: 128, y: 240, t: 'w' },
-				{ x: 200, y: 52, t: 'h' }
-			],
-			[
-				{ x: 78, y: 352, t: 'h' },
-				{ x: 78, y: 320, t: 'w' },
-				{ x: 0, y: 240, t: 'w' },
-				{ x: 192, y: 240, t: 'w' },
-				{ x: 30, y: 150, t: 'w' },
-				{ x: 158, y: 150, t: 'w' }
-			],
-			[
-				{ x: 188, y: 352, t: 'h' },
-				{ x: 92, y: 320, t: 'w' },
-				{ x: 0, y: 240, t: 'w' },
-				{ x: 128, y: 240, t: 'w' },
-				{ x: 256, y: 240, t: 'h' },
-				{ x: 180, y: 52, t: 'h' },
-				{ x: 52, y: 148, t: 'w' }
-			]
-		];
-		for(var i=0; i<this.maxLevels; i++) {
-			var newLevel = this.add.group();
-			newLevel.enableBody = true;
-			newLevel.physicsBodyType = Phaser.Physics.ARCADE;
-			for(var e=0; e<this.levelData[i].length; e++) {
-				var item = this.levelData[i][e];
-				newLevel.create(item.x, item.y, 'element-'+item.t);
-			}
-			newLevel.setAll('body.immovable', true);
-			newLevel.visible = false;
-			this.levels.push(newLevel);
-		}
-	},
-	showLevel: function(level) {
-		var lvl = level | this.level;
-		if(this.levels[lvl-2]) {
-			this.levels[lvl-2].visible = false;
-		}
-		this.levels[lvl-1].visible = true;
-	},
 	updateCounter: function() {
 		this.timer++;
 		this.timerText.setText("Time: "+this.timer);
 		this.totalTimeText.setText("Total time: "+(this.totalTimer+this.timer));
-	},
-	managePause: function() {
-		this.game.paused = true;
-		var pausedText = this.add.text(Ball._WIDTH*0.5, 250, "Game paused,\ntap anywhere to continue.", this.fontMessage);
-		pausedText.anchor.set(0.5);
-		this.input.onDown.add(function(){
-			pausedText.destroy();
-			this.game.paused = false;
-		}, this);
 	},
 	manageAudio: function() {
 		this.audioStatus =! this.audioStatus;
 		this.audioButton.animations.play(this.audioStatus);
 	},
 	update: function() {
-		if(this.keys.left.isDown) {
-			this.ball.body.velocity.x -= this.movementForce;
-		}
-		else if(this.keys.right.isDown) {
-			this.ball.body.velocity.x += this.movementForce;
-		}
-		if(this.keys.up.isDown) {
-			this.ball.body.velocity.y -= this.movementForce;
-		}
-		else if(this.keys.down.isDown) {
-			this.ball.body.velocity.y += this.movementForce;
-		}
-		this.physics.arcade.collide(this.ball, this.borderGroup, this.wallCollision, null, this);
-		this.physics.arcade.collide(this.ball, this.levels[this.level-1], this.wallCollision, null, this);
-		this.physics.arcade.overlap(this.ball, this.hole, this.finishLevel, null, this);
+
 	},
 	wallCollision: function() {
 		if(this.audioStatus) {
@@ -159,30 +76,8 @@ Ball.Game.prototype = {
 		var x = e.gamma; // range [-90,90], left-right
 		var y = e.beta;  // range [-180,180], top-bottom
 		var z = e.alpha; // range [0,360], up-down
-		Ball._player.body.velocity.x += x;
-		Ball._player.body.velocity.y += y*0.5;
 	},
-	finishLevel: function() {
-		if(this.level >= this.maxLevels) {
-			this.totalTimer += this.timer;
-			alert('Congratulations, game completed!\nTotal time of play: '+this.totalTimer+' seconds!');
-			this.game.state.start('MainMenu');
-		}
-		else {
-			alert('Congratulations, level '+this.level+' completed!');
-			this.totalTimer += this.timer;
-			this.timer = 0;
-			this.level++;
-			this.timerText.setText("Time: "+this.timer);
-			this.totalTimeText.setText("Total time: "+this.totalTimer);
-			this.levelText.setText("Level: "+this.level+" / "+this.maxLevels);
-			this.ball.body.x = this.ballStartPos.x;
-			this.ball.body.y = this.ballStartPos.y;
-			this.ball.body.velocity.x = 0;
-			this.ball.body.velocity.y = 0;
-			this.showLevel();
-		}
-	},
+
 	render: function() {
 		// this.game.debug.body(this.ball);
 		// this.game.debug.body(this.hole);
