f5316a96ff79b50825685d91f2ba96a3cd4acb8a
Unknown <nicobighetti@gmail.com>
2018-01-18 20:27:22 +0100

    just enough ocd

--- www/src/utils/EventRegistry.js
+++ www/src/utils/EventRegistry.js
@@ -1,6 +1,6 @@
 //Register all phaser signal used with broadcasted event
 function EventRegistry(){
-    
+
     this.relaySignal = new Phaser.Signal();
 
     this.init = function(){
@@ -8,16 +8,16 @@ function EventRegistry(){
       FastGame.fastSocket.addBroadcastCallback(PROTOCOL.FAST_EVENT_BROADCAST, this.relaySignal);
       this.callbackArray = [];
     };
-    
+
     //keyEvent : the event the callback must react to
     this.addCallback = function(keyEvent, callback){
       this.callbackArray[keyEvent] = callback;
     }
-    
+
     this.removeCallback = function(keyEvent){
       this.callbackArray[keyEvent] = undefined;
     }
-    
+
     //The server response on event broadcast
     this.eventBroadcastCallback = function(response){
       var data = response.data;
