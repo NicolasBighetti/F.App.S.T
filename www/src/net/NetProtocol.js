651ab29c37091ccb4fb45d1c4aaf7a3378da52e4
Unknown <nicobighetti@gmail.com>
2018-01-18 21:46:48 +0100

    comment and cleaning

--- www/src/net/NetProtocol.js
+++ www/src/net/NetProtocol.js
@@ -1,6 +1,7 @@
 var PROTOCOL = new NetworkEventBundle();
 
 function NetworkEventBundle(){
+  //Change the variable value but not the variable name or you'll break stuff 
   //For testing purpose, to be refacctored at a later date
   this.FAST_MINI_GAME_REGISTER = 'FAST_MINI_GAME_REGISTER';
   this.FAST_EVENT_BROADCAST = 'FAST_EVENT_BROADCAST';
