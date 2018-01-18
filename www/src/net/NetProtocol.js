7d2dbcd5b900470218c69756ce47a6782486bcf8
Unknown <nicobighetti@gmail.com>
2018-01-18 10:09:32 +0100

    clean up NetProtocol

--- www/src/net/NetProtocol.js
+++ www/src/net/NetProtocol.js
@@ -1,9 +1,10 @@
 var PROTOCOL = new NetworkEventBundle();
 
 function NetworkEventBundle(){
+  //For testing purpose, to be refacctored at a later date
   this.FAST_MINI_GAME_REGISTER = 'FAST_MINI_GAME_REGISTER';
   this.FAST_EVENT_BROADCAST = 'FAST_EVENT_BROADCAST';
-  this.TEST = 'message';
+
 
   this.getEmitableEvent = function(){
     return [
