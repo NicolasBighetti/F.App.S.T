0020c4811c0a4fb11bce9cc02838acbabb201372
Unknown <nicobighetti@gmail.com>
2018-01-18 11:30:48 +0100

    add broadcast_event to protocol

--- www/src/net/NetProtocol.js
+++ www/src/net/NetProtocol.js
@@ -5,11 +5,30 @@ function NetworkEventBundle(){
   this.FAST_MINI_GAME_REGISTER = 'FAST_MINI_GAME_REGISTER';
   this.FAST_EVENT_BROADCAST = 'FAST_EVENT_BROADCAST';
 
+  //Broadcast event PROTOCOL
+  this.FAST_EVENT_VIBRATION_WEAK = 'FAST_EVENT_VIBRATION_WEAK';
+  this.FAST_EVENT_VIBRATION_MEDIUM = 'FAST_EVENT_VIBRATION_MEDIUM';
+  this.FAST_EVENT_VIBRATION_STRONG = 'FAST_EVENT_VIBRATION_STRONG';
+  this.FAST_EVENT_LIGHT_OFF = 'FAST_EVENT_LIGHT_OFF';
+  this.FAST_EVENT_LIGHT_ON = 'FAST_EVENT_LIGHT_ON';
+  this.FAST_EVENT_SOUND_EFFECT = 'FAST_EVENT_SOUND_EFFECT';
+
 
   this.getEmitableEvent = function(){
     return [
-      this.FAST_EVENT_BROADCAST,
-      this.TEST
+      this.FAST_EVENT_BROADCAST
     ];
   }
+
+  this.getBroadcastedEvent = function(){
+    return [
+      this.FAST_EVENT_VIBRATION_WEAK,
+      this.FAST_EVENT_VIBRATION_MEDIUM,
+      this.FAST_EVENT_VIBRATION_STRONG,
+      this.FAST_EVENT_LIGHT_OFF,
+      this.FAST_EVENT_LIGHT_ON,
+      this.FAST_EVENT_SOUND_EFFECT
+    ];
+  }
+
 }
