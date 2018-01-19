907a758dfee8908c1254e03a7516990521a166df
Unknown <nicobighetti@gmail.com>
2018-01-19 10:34:21 +0100

    Add signal registry
    register signal to network event, add custom signal callback

--- www/src/net/NetProtocol.js
+++ www/src/net/NetProtocol.js
@@ -1,10 +1,12 @@
 var PROTOCOL = new NetworkEventBundle();
 
 function NetworkEventBundle(){
-  //Change the variable value but not the variable name or you'll break stuff 
-  //For testing purpose, to be refacctored at a later date
+  //Change the variable value but not the variable name or you'll break stuff
   this.FAST_MINI_GAME_REGISTER = 'FAST_MINI_GAME_REGISTER';
   this.FAST_EVENT_BROADCAST = 'FAST_EVENT_BROADCAST';
+  this.FAST_PRIVATE_MINI_GAME_START = 'FAST_PRIVATE_MINI_GAME_START';
+
+  this.FAST_PHONE_CONNECT = 'FAST_PHONE_CONNECT';
 
   //Broadcast event PROTOCOL
   this.FAST_EVENT_VIBRATION_WEAK = 'FAST_EVENT_VIBRATION_WEAK';
@@ -14,10 +16,18 @@ function NetworkEventBundle(){
   this.FAST_EVENT_LIGHT_ON = 'FAST_EVENT_LIGHT_ON';
   this.FAST_EVENT_SOUND_EFFECT = 'FAST_EVENT_SOUND_EFFECT';
 
+  
 
   this.getEmitableEvent = function(){
     return [
-      this.FAST_EVENT_BROADCAST
+      this.FAST_EVENT_BROADCAST,
+      this.FAST_PHONE_CONNECT
+    ];
+  }
+
+  this.getReactableEvent = function(){
+    return [
+      this.FAST_PRIVATE_MINI_GAME_START
     ];
   }
 
