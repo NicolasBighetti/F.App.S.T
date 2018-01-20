3e86a32eef8b792dea3dbfe796ab8bb6f2e45ebb
Unknown <nicobighetti@gmail.com>
2018-01-20 21:04:00 +0100

    add decibel meter wrapper

---
+++ www/src/utils/cordova/DecibelMeter.js
@@ -0,0 +1,39 @@
+function DecibelMeter(){
+
+  this.decibelSignal = new Phaser.Signal();
+
+  var _this = this;
+
+  DBMeter.start(
+    function(db){
+      _this.decibelSignal.dispatch(db);
+    },
+    function(err){
+      console.log('Decibel error : ' + err );
+    }
+  );
+
+  this.subscribe = function(callback, context){
+    this.decibelSignal.add(callback, context);
+  }
+
+  this.destroy = function(){
+    DBMeter.stop(
+      function(){
+        //If it is stopped we can kill it
+        DBMeter.delete(
+          function(){
+            console.log('destroyed all right');
+          },
+          function(err){
+            console.log('really shouldnt happen');
+          }
+        );
+      },
+      function(err){
+        console.log('probably not launched anyway');
+      }
+    );
+  }
+
+}
