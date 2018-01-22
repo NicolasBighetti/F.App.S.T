1f54abc530e641a7dc18332353911077d57651c4
Flavian Jacquot <flavian.jacquot@gmail.com>
2018-01-22 14:14:51 +0100

    color connect tests

---
+++ www/src/gamestates/ColorConnector.js
@@ -0,0 +1,90 @@
+﻿
+FastGame.ColorConnector = function (game) { this.game=game};
+FastGame.ColorConnector.prototype = {
+    preload: function () {
+        this.video = undefined;
+    },
+    create: function () {
+        console.log("create fast color");
+        this.video = this.game.add.video();
+
+        //  If access to the camera is allowed
+        this.video.onAccess.add(this.camAllowed, this);
+
+        //  If access to the camera is denied
+        this.video.onError.add(this.camBlocked, this);
+
+        //  As soon as the stream starts this will fire
+        this.video.onChangeSource.add(this.takeSnapshot, this);
+
+        //  Start the stream
+        this.video.startMediaStream();
+
+    },
+    update: function () {
+
+    },
+    camAllowed: function () {
+        console.log('Camera was allowed', this.video);
+
+        var cam = this.video.addToWorld();
+        cam.scale.set(0.2);
+
+        grab = this.video.snapshot.addToWorld(this.game.width, this.game.height);
+        grab.scale.set(0.2);
+
+        // game.add.text(400, 32, 'Click to grab', { font: "bold 26px Arial", fill: "#ffffff" })
+
+        this.game.input.onDown.add(this.takeSnapshot, this);
+
+        //game.time.events.loop(50, takeSnapshot, this);
+
+    },
+
+camBlocked: function (video, error) {
+
+        console.log('Camera was blocked', video, error);
+
+    },
+takeSnapshot: function () {
+        this.video.grab();//params clear true or false, alpha, blend mode
+        // if()
+        // console.log(video.snapshot);
+        // bmd.draw(video.snapshot);
+        this.video.snapshot.update();
+        var color = this.video.snapshot.getPixelRGB(this.video.snapshot.width / 2, this.video.snapshot.height/2);
+        console.log(color);
+        var colorServ = {
+            red: color.r,
+            green: color.g,
+            blue: color.b
+        }
+        console.log(colorServ);
+
+        //this.sendColor(colorServ);
+        console.log('sending color' + colorServ);
+        var byte = this.extractdata(colorServ);
+        console.log('byte decoded' + byte);
+        colorServ.byteO = byte;
+        FastGame.fastSocket.EMIT.FAST_COLOR(colorServ);
+
+
+},
+    extractdata: function (color) {
+        // use only 2 strongest bits for each component for now ...
+        var byte = 255;
+
+        var r = color.red >> 7;
+        var g = color.green >> 7 << 1;
+        var b = color.blue >> 7 << 2;
+        byte = 255 & (r | g | b);
+ 
+        console.log(r); // 1
+        console.log(g); // 1 
+        console.log(b); // 1
+        console.log("byte");
+        console.log(byte); // 1
+
+        return byte;
+    }
+};
--- www/src/net/NetProtocol.js
+++ www/src/net/NetProtocol.js
@@ -7,6 +7,7 @@ function NetworkEventBundle(){
   this.FAST_PRIVATE_MINI_GAME_START = 'FAST_PRIVATE_MINI_GAME_START';
 
   this.FAST_PHONE_CONNECT = 'FAST_PHONE_CONNECT';
+  this.FAST_COLOR = 'FAST_COLOR';
 
   //Broadcast event PROTOCOL
   this.FAST_EVENT_VIBRATION_WEAK = 'FAST_EVENT_VIBRATION_WEAK';
@@ -16,12 +17,14 @@ function NetworkEventBundle(){
   this.FAST_EVENT_LIGHT_ON = 'FAST_EVENT_LIGHT_ON';
   this.FAST_EVENT_SOUND_EFFECT = 'FAST_EVENT_SOUND_EFFECT';
 
+
   
 
   this.getEmitableEvent = function(){
     return [
       this.FAST_EVENT_BROADCAST,
-      this.FAST_PHONE_CONNECT
+      this.FAST_PHONE_CONNECT,
+      this.FAST_COLOR
     ];
   }
 
